f81a82c433f6aabeec86cc0f96329b0d00f392a1
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2023-12-30 23:33:10 +0900

    Add in device preview (#2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,5 +10,8 @@ import 'package:schedulenus/src/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(DevicePreview(
+    builder: (context) => const ProviderScope(child: MyApp()),
+    enabled: !kReleaseMode,
+  ));
 }
