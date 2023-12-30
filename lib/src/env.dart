e92394805db411dfd6b447cb0de94062955983b7
woowenjun99 <woowenjun99@gmail.com>
2023-12-30 23:05:59 +0900

    Remove unused files

--- lib/src/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part 'env.g.dart';
-
-@Envied()
-final class Env {
-  @EnviedField(varName: 'SERVER_URL', obfuscate: true)
-  static final String serverUrl = _Env.serverUrl;
-}
