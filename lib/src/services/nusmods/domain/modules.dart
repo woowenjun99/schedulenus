81831e001675d4d065384ce6ef84ac4da797e2ad
woowenjun99 <woowenjun99@gmail.com>
2024-01-04 14:14:08 +0800

    Set up the reviews tab

--- lib/src/services/nusmods/domain/modules.dart
+++ lib/src/services/nusmods/domain/modules.dart
@@ -1,11 +1,13 @@
 class Module {
   final String description;
+  final String department;
   final String moduleCode;
   final String moduleCredit;
   final String title;
 
   const Module({
     required this.description,
+    required this.department,
     required this.moduleCode,
     required this.moduleCredit,
     required this.title,
@@ -14,6 +16,7 @@ class Module {
   factory Module.fromJson(Map<String, dynamic> json) {
     return Module(
       description: json["description"],
+      department: json["department"],
       moduleCredit: json["moduleCredit"],
       moduleCode: json["moduleCode"],
       title: json["title"],
--- lib/src/services/nusmods/presentation/individual_module_screen.dart
+++ lib/src/services/nusmods/presentation/individual_module_screen.dart
@@ -1,12 +1,10 @@
 import "package:auto_size_text/auto_size_text.dart";
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:google_fonts/google_fonts.dart";
-import "package:readmore/readmore.dart";
 
-import "package:schedulenus/src/common_widgets/button.dart";
 import "package:schedulenus/src/services/nusmods/data/nusmods_repository.dart";
 import "package:schedulenus/src/services/nusmods/domain/modules.dart";
+import "package:schedulenus/src/services/nusmods/presentation/individual_module_info.dart";
 
 class IndividualModuleScreen extends ConsumerStatefulWidget {
   final String moduleCode;
@@ -43,98 +41,8 @@ class _IndividualModuleScreenState
             loading: () => const Center(child: CircularProgressIndicator()),
             data: (data) {
               setState(() => hasLoaded = true);
-              return IndividualModule(module: data);
+              return IndividualModuleInfo(module: data);
             }),
-      ),
-      bottomSheet: hasLoaded
-          ? Container(
-              padding: const EdgeInsets.all(12),
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  PrimaryButton(
-                    onPressed: () {},
-                    isLoading: false,
-                    buttonText: "Enroll",
-                  ),
-                ],
-              ),
-            )
-          : null,
-    );
-  }
-}
-
-class IndividualModule extends StatelessWidget {
-  final Module module;
-
-  const IndividualModule({
-    required this.module,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * 0.05,
-              vertical: 8,
-            ),
-            child: AutoSizeText(
-              module.title,
-              style: GoogleFonts.inter(
-                fontWeight: FontWeight.bold,
-              ),
-              minFontSize: 20,
-              overflow: TextOverflow.clip,
-            ),
-          ),
-          const TabBar(tabs: [
-            Tab(text: "About"),
-          ]),
-          Flexible(
-            child: TabBarView(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: MediaQuery.of(context).size.width * 0.05,
-                    vertical: 8,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AutoSizeText(
-                        "Description",
-                        style: GoogleFonts.inter(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      ReadMoreText(
-                        module.description,
-                        trimExpandedText: "Read less",
-                        trimCollapsedText: "Read more",
-                        moreStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        lessStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
